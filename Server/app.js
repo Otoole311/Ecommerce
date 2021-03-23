ca6b37af2827b51631603da44d7f1cd5db3e814b
Otoole311 <50079783+Otoole311@users.noreply.github.com>
2021-03-23 11:09:07 +0000

    First Commit

---
+++ Server/app.js
@@ -0,0 +1,34 @@
+//requires
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./src/database/db');
+const { createServer } = require('http');
+
+//assignments
+const app = express();
+const port = process.env.PORT || 3000;
+
+//app sets
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+//app use
+app.use(express.static(path.join(__dirname, '/src/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+//db connection
+db().then(() => createServer(app).listen(port, ()=>{
+    debug(`listening on port + ${chalk.green(port)}`);
+})).catch((err) =>{
+});
---
+++ Server/src/database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+module.exports = function () {
+   // const { MONGO_CLUSTER_URL = 'mongodb://localhost:27017', MONGO_DATABASE_NAME } = process.env;
+
+    // Configure mongoose to use Promises, because callbacks are passe.
+    mongoose.Promise = global.Promise;
+    // Connect to the Mongo DB
+    return mongoose.connect('mongodb://localhost:27017/Commerce', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    });
+};
