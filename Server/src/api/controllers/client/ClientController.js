abf044f5ac09f6664ee4d3aa6f7c7a8b4f6facd6
Otoole311 <50079783+Otoole311@users.noreply.github.com>
2021-03-27 21:49:21 +0000

    updates to client controller

---
+++ Server/src/api/controllers/client/ClientController.js
@@ -0,0 +1,20 @@
+/**
+ * Functions to implement
+ * 
+ * View all categories
+ * View all subcategories.
+ * View all products.
+ * 
+ * Select a category
+ * Select a subcategroy
+ * select a product
+ * 
+ * Add product to cart
+ * 
+ * Check out item
+ * Check out all items
+ * 
+ * payment
+ * 
+ */
+
