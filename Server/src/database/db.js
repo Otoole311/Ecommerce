cf6152eeed79073fcf0b52afb0f171157965b83a
Otoole311 <50079783+Otoole311@users.noreply.github.com>
2021-03-31 19:39:33 +0000

    Changes to database file

--- Server/src/database/db.js
+++ Server/src/database/db.js
@@ -1,4 +1,15 @@
 const mongoose = require('mongoose');
+const {config} = require('dotenv');
+const debug = require('debug')('app:db.js');
+
+config();
+const {NODE_ENV, DEV_DB} = process.env;
+const options = {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+}
 
 module.exports = function () {
    // const { MONGO_CLUSTER_URL = 'mongodb://localhost:27017', MONGO_DATABASE_NAME } = process.env;
@@ -6,11 +17,14 @@ module.exports = function () {
     // Configure mongoose to use Promises, because callbacks are passe.
     mongoose.Promise = global.Promise;
     // Connect to the Mongo DB
-    return mongoose.connect('mongodb://localhost:27017/Commerce', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    }).then(() => console.log("Connected to Database."))
-    .catch(err => console.error("Connection error", err));
+    return mongoose.connect(DEV_DB, options)
+    .then(() => {
+        debug("Connected to Database.")
+        //console.log("Connected to Database.")
+    })
+    .catch(err => {
+        debug("Connection error")
+        debug(err)
+        //console.error("Connection error", err)
+    });
 };
